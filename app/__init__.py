14d9e9681569e206a02d0f9eb66515bbe804c1a5
stephan542 <32601168+stephan542@users.noreply.github.com>
2018-03-28 20:24:28 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "P@$$W0rd"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://udpnroqnifkdui:30565a11c7ad9fdba7f63ec81fb3e91ed508231508a8734f8844b8fab2a6cbbf@ec2-107-21-126-193.compute-1.amazonaws.com:5432/d1afs04f48a935'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://udpnroqnifkdui:30565a11c7ad9fdba7f63ec81fb3e91ed508231508a8734f8844b8fab2a6cbbf@ec2-107-21-126-193.compute-1.amazonaws.com:5432/d1afs04f48a935'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  
 db = SQLAlchemy(app)
 
